8a4fec5e75e546d74cca24af46ffaa08cfb05795
abhishek <abhisheksingh4312@gmail.com>
2018-07-30 23:33:54 +0530

    initial commit

---
+++ src/app/movie.ts
@@ -0,0 +1,9 @@
+export class Movie {
+    constructor(
+        public title:string,
+        public year:string,
+        public imdbId:string,
+        public type:string,
+        public poster:string
+    ){}
+}
